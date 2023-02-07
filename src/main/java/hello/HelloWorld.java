27e9a03aaf50a9458bbc49312bfe7967eea28ec4
Christina Antonuccio <antonuccio.christina@gmail.com>
2023-02-07 10:16:22 -0600

    update gitignore

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,2 +1,12 @@
-package hello;public class HelloWorld {
+package hello;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        //the program starts here
+        System.out.println("Hello world");
+        //sout tab to create above line
+        //same as console.log
+        //dont need a browser to run java, we can run it right here in the command line
+    }
 }
